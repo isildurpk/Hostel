33868a75c886bf0e14574408005dae108c579aca
Egor <isildurpk@gmail.com>
2015-12-21 13:46:31 +0700

    Открытие списка студентов. Загрузка данных

--- Hostel/HostelPortable/ViewModels/MainVm.cs
+++ Hostel/HostelPortable/ViewModels/MainVm.cs
@@ -1,6 +1,10 @@
-﻿using MugenMvvmToolkit;
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using HostelPortable.ViewModels.Students;
+using MugenMvvmToolkit;
 using MugenMvvmToolkit.Infrastructure.Presenters;
 using MugenMvvmToolkit.Interfaces.Presenters;
+using MugenMvvmToolkit.Models;
 using MugenMvvmToolkit.ViewModels;
 
 namespace HostelPortable.ViewModels
@@ -13,6 +17,26 @@ namespace HostelPortable.ViewModels
         {
             Should.NotBeNull(viewModelPresenter, nameof(viewModelPresenter));
             viewModelPresenter.DynamicPresenters.Add(new DynamicMultiViewModelPresenter(this));
+
+            OpenStudentsCommand = RelayCommandBase.FromAsyncHandler(OpenStudentsAsync);
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand OpenStudentsCommand { get; private set; }
+
+        #endregion
+
+        #region Command`s methods
+
+        private async Task OpenStudentsAsync()
+        {
+            using (var vm = GetViewModel<StudentWorkspaceVm>())
+            {
+                await vm.ShowAsync();
+            }
         }
 
         #endregion
--- Hostel/HostelPortable/ViewModels/Students/StudentWorkspaceVm.cs
+++ Hostel/HostelPortable/ViewModels/Students/StudentWorkspaceVm.cs
@@ -1,30 +1,54 @@
 ﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using HostelPortable.Interfaces;
 using HostelPortable.Projections;
+using MugenMvvmToolkit;
+using MugenMvvmToolkit.Models;
 using MugenMvvmToolkit.ViewModels;
 
 namespace HostelPortable.ViewModels.Students
 {
     public sealed class StudentWorkspaceVm : WorkspaceViewModel
     {
+        private readonly IRepository _repository;
+
         #region Fields
 
         private Task _initializedTask;
 
         #endregion
 
-
         #region Constructors
 
-        public StudentWorkspaceVm()
+        public StudentWorkspaceVm(IRepository repository)
         {
+            _repository = repository;
+
             DisplayName = UiResources.StudentWorkspaceName;
+
+            RefreshCommand = RelayCommandBase.FromAsyncHandler(RefreshAsync);
         }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand RefreshCommand { get; private set; }
 
         #endregion
 
         #region Properties
 
         public GridViewModel<StudentProjection> StudentsVm { get; private set; }
+
+        #endregion
+
+        #region Command`s methods
+
+        private async Task RefreshAsync()
+        {
+            await _initializedTask;
+        }
 
         #endregion
 
@@ -36,7 +60,8 @@ namespace HostelPortable.ViewModels.Students
 
             StudentsVm = GetViewModel<GridViewModel<StudentProjection>>();
 
-            
+            _initializedTask = _repository.LoadStudentProjectionsAsync().TryExecuteSynchronously(task => StudentsVm.UpdateItemsSource(task.Result))
+                .WithBusyIndicator(this);
         }
 
         #endregion
