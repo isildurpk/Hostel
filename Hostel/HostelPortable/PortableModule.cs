eeb46cd6d54a5d4325d8ad9134713f15a426b365
Egor <isildurpk@gmail.com>
2015-12-21 12:12:51 +0700

    [+] PortableModule

--- Hostel/HostelPortable/PortableModule.cs
+++ Hostel/HostelPortable/PortableModule.cs
@@ -1,6 +1,36 @@
-﻿namespace HostelPortable
+﻿using MugenMvvmToolkit;
+using MugenMvvmToolkit.Interfaces.Models;
+using MugenMvvmToolkit.Models;
+using MugenMvvmToolkit.Modules;
+
+namespace HostelPortable
 {
-    public class PortableModule
+    public class PortableModule : ModuleBase
     {
+        #region Constructors
+
+        public PortableModule() : base(false, LoadMode.All)
+        {
+        }
+
+        #endregion
+
+        #region Overrides of ModuleBase
+
+        protected override bool LoadInternal()
+        {
+            IocContainer.BindToConstant<IViewModelSettings>(new DefaultViewModelSettings
+            {
+                DefaultBusyMessage = UiResources.DefaultBusyMessage
+            });
+
+            return true;
+        }
+
+        protected override void UnloadInternal()
+        {
+        }
+
+        #endregion
     }
 }
